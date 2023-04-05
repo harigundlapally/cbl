6e8ab25cd15047f04df25cd8e21526762f0f69f5
harigundlapally <32592457+harigundlapally@users.noreply.github.com>
2023-04-05 15:03:41 +0530

    Update Onboarding.tsx

--- src/app/modules/subscriptions/pages/Onboarding.tsx
+++ src/app/modules/subscriptions/pages/Onboarding.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import OnboardingForm from '../components/OnboardingForm'
 import OnboardingProduct from '../components/OnboardingProduct'
 
 const Onboarding = () => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0)
+    }, 10);
+  }, []);
+
   return (
-    <div className='d-flex flex-column flex-lg-row flex-column-fluid min-vh-100'>
-      <OnboardingProduct />
-      <OnboardingForm />
+    <div className='container'>
+      <div className="row">
+        <div className="col-md-6">
+
+          <OnboardingProduct />
+        </div>
+        <div className="col-md-6">
+          <OnboardingForm />
+
+        </div>
+      </div>
     </div>
   )
 }
